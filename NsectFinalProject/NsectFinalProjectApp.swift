37938bd262889af41eedeabc07fba7c491207c1f
Davi Viana <d.silva.viana.red@gmail.com>
2025-07-04 16:47:04 -0300

    Initial Commit

---
+++ NsectFinalProject/NsectFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NsectFinalProjectApp.swift
+//  NsectFinalProject
+//
+//  Created by found on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct NsectFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
