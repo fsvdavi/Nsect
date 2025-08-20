99422a8cc3a7446d760da15fff3f79fe16b78dfa
Franco <gustavo.ramos62@aluno.ifce.edu.br>
2025-08-20 16:56:16 -0300

    .

--- NsectFinalProject/NsectFinalProjectApp.swift
+++ NsectFinalProject/NsectFinalProjectApp.swift
@@ -5,13 +5,48 @@
 //  Created by found on 04/07/25.
 //
 
+
 import SwiftUI
 
+struct SplashView: View {
+    @State private var isActive = false
+
+    var body: some View {
+        if isActive {
+            MainView() // depois vai para sua tela principal
+        } else {
+            VStack {
+                Image("nsectLogo") // sua logo
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+
+                Image("nsectTitle") // nome do app
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 60)
+                    .padding(.top, 16)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white) // cor de fundo da intro
+            .ignoresSafeArea()
+            .onAppear {
+                // tempo que a tela fica visível antes de mudar
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+
 @main
-struct NsectFinalProjectApp: App {
+struct MeuApp: App {
     var body: some Scene {
         WindowGroup {
-            MainView()
+            SplashView()
         }
     }
 }
